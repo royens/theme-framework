a0075063ada94779f35f4466382acd7d1e3649c2
Roy Ens <Roy.Ens@gmail.com>
2011-08-04 01:43:57 -0400

    Added new IE class to conditional html start.

--- header.php
+++ header.php
@@ -11,13 +11,13 @@
 ?>
 <!doctype html>
 <!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
+<html id="ie ie6" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
+<html id="ie ie7" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
+<html id="ie ie8" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if !(IE 6) | !(IE 7) | !(IE 8) ]><!-->
 <html <?php language_attributes(); ?>>
